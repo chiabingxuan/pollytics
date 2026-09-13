82a78548ffe8efd6a4c05de84dccff7eeb05f32f
chiabingxuan <bingxuan2039@gmail.com>
2026-09-14 00:12:34 +0800

    feat: Rework schema to include division and region entities

--- src/backend/sql/schema.sql
+++ src/backend/sql/schema.sql
@@ -5,27 +5,46 @@ CREATE TABLE IF NOT EXISTS countries (
     name VARCHAR(256) UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS country_borders (
-    country_id INT REFERENCES countries(id),
-    year INT,
-    geometry GEOMETRY(MULTIPOLYGON, 4326) NOT NULL,
-    PRIMARY KEY (country_id, year)
+/*
+Represents electoral divisions, within which electoral contests take place.
+
+Eg. country, state, congressional district, constituency.
+*/
+CREATE TABLE IF NOT EXISTS divisions (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    country_id INT NOT NULL REFERENCES countries(id),
+    type VARCHAR(256) NOT NULL CHECK (type IN ('country', 'state', 'cd', 'constituency')), -- a country can also a division at-large
+    UNIQUE (name, country_id)
 );
 
+CREATE TABLE IF NOT EXISTS division_borders (
+    division_id INT REFERENCES divisions(id),
+    year_drawn INT,
+    geometry GEOMETRY(MULTIPOLYGON, 4326) NOT NULL,
+    PRIMARY KEY (division_id, year_drawn)
+);
+
+/*
+Regional results may be available for a division.
+The same region may appear in multiple divisions,
+and the division-region relationship may vary across elections.
+
+Eg. A state has many counties; a congressional district has a few counties.
+Note that the same county may be in multiple congressional districts.
+*/
 CREATE TABLE IF NOT EXISTS regions (
     id SERIAL PRIMARY KEY,
     name VARCHAR(256) NOT NULL,
     country_id INT NOT NULL REFERENCES countries(id),
-    parent_region_id INT NOT NULL REFERENCES regions(id), -- a region will be a child of itself if there is nothing bigger
-    type VARCHAR(256) NOT NULL CHECK (type IN ('state', 'county')),
-    UNIQUE (name, parent_region_id)
+    type VARCHAR(256) NOT NULL CHECK (type IN ('county'))
 );
 
 CREATE TABLE IF NOT EXISTS region_borders (
     region_id INT REFERENCES regions(id),
-    year INT,
+    year_drawn INT,
     geometry GEOMETRY(MULTIPOLYGON, 4326) NOT NULL,
-    PRIMARY KEY (region_id, year)
+    PRIMARY KEY (region_id, year_drawn)
 );
 
 CREATE TABLE IF NOT EXISTS candidates (
@@ -44,38 +63,85 @@ CREATE TABLE IF NOT EXISTS parties (
 CREATE TABLE IF NOT EXISTS elections (
     id SERIAL PRIMARY KEY,
     country_id INT NOT NULL REFERENCES countries(id),
+    name VARCHAR(256) NOT NULL,
     year INT NOT NULL,
     type VARCHAR(64) NOT NULL CHECK (type IN ('presidential', 'senate', 'house', 'gubernatorial')),
-    UNIQUE (country_id, year, type)
+    UNIQUE (country_id, name, year)
 );
 
-CREATE TABLE IF NOT EXISTS candidacies (
-    id SERIAL PRIMARY KEY,
-    election_id INT NOT NULL REFERENCES elections(id),
-    candidate_id INT NOT NULL REFERENCES candidates(id),
-    UNIQUE (election_id, candidate_id)
-);
-
-
-CREATE TABLE IF NOT EXISTS candidacy_parties (
-    candidacy_id INT PRIMARY KEY REFERENCES candidacies(id),
-    party_id INT NOT NULL REFERENCES parties(id)
-);
-
--- for vote counts of the candidacies recorded
-CREATE TABLE IF NOT EXISTS main_results (
-    candidacy_id INT REFERENCES candidacies(id),
-    region_id INT REFERENCES regions(id),
-    votes INT NOT NULL CHECK (votes >= 0),
-    PRIMARY KEY (candidacy_id, region_id)
-);
-
--- for vote counts outside of the main candidacies
--- eg. other minor candidates, spoiled votes, etc.
-CREATE TABLE IF NOT EXISTS other_results (
+/*
+Represents individual electoral contests, each corresponding to a (election, division) pair.
+Eg. house race for the 2020 US election in Michigan's 1st district.
+*/
+CREATE TABLE IF NOT EXISTS races (
     election_id INT REFERENCES elections(id),
+    division_id INT REFERENCES divisions(id),
+    PRIMARY KEY (election_id, division_id)
+);
+
+/*
+Represents candidates participating in races.
+*/
+CREATE TABLE IF NOT EXISTS participations (
+    candidate_id INT REFERENCES candidates(id),
+    election_id INT,
+    division_id INT,
+    total_votes INT NOT NULL CHECK (total_votes >= 0),
+    PRIMARY KEY (candidate_id, election_id, division_id),
+    FOREIGN KEY (election_id, division_id) REFERENCES races(election_id, division_id)
+);
+
+CREATE TABLE IF NOT EXISTS participation_parties (
+    candidate_id INT,
+    election_id INT,
+    division_id INT, 
+    party_id INT NOT NULL REFERENCES parties(id),
+    PRIMARY KEY (candidate_id, election_id, division_id),
+    FOREIGN KEY (candidate_id, election_id, division_id) REFERENCES participations(candidate_id, election_id, division_id)
+);
+
+/*
+Represents votes that are not for the known participations.
+Eg. other minor candidates, spoiled votes, etc.
+*/
+CREATE TABLE IF NOT EXISTS other_vote_types (
+    type VARCHAR(64) PRIMARY KEY CHECK (type IN ('other_cands', 'spoiled', 'undervotes', 'overvotes'))
+);
+
+/*
+Represents race-wide vote counts outside of the known participations.
+*/
+CREATE TABLE IF NOT EXISTS other_racewide_results (
+    election_id INT,
+    division_id INT,
+    type VARCHAR(64) REFERENCES other_vote_types(type),
+    votes INT NOT NULL CHECK (votes >= 0),
+    PRIMARY KEY (election_id, division_id, type),
+    FOREIGN KEY (election_id, division_id) REFERENCES races(election_id, division_id)
+);
+
+/*
+Represents participation results by region.
+*/
+CREATE TABLE IF NOT EXISTS participation_regional_results (
+    candidate_id INT,
+    election_id INT,
+    division_id INT,
     region_id INT REFERENCES regions(id),
     votes INT NOT NULL CHECK (votes >= 0),
-    type VARCHAR(64) CHECK (type IN ('other_cands', 'spoiled', 'undervotes', 'overvotes')),
-    PRIMARY KEY (election_id, region_id, type)
+    PRIMARY KEY (candidate_id, election_id, division_id, region_id),
+    FOREIGN KEY (candidate_id, election_id, division_id) REFERENCES participations(candidate_id, election_id, division_id)
+);
+
+/*
+Represents race-wide vote counts outside of the known participations, by region.
+*/
+CREATE TABLE IF NOT EXISTS other_regional_results (
+    election_id INT,
+    division_id INT,
+    region_id INT REFERENCES regions(id),
+    type VARCHAR(64) REFERENCES other_vote_types(type),
+    votes INT NOT NULL CHECK (votes >= 0),
+    PRIMARY KEY (election_id, division_id, region_id, type),
+    FOREIGN KEY (election_id, division_id) REFERENCES races(election_id, division_id)
 );
